efdaf69a20dde637529d4f167ad3fe77d37eec77
samigas <163407588+samigas@users.noreply.github.com>
2024-03-16 00:12:27 +0900

    Create hw1.py

---
+++ hw1.py
@@ -0,0 +1,13 @@
+def get_radius(prompt):
+    r = int(input(prompt))
+    return r
+
+def get_circle_area(radius):
+    area = 3.14 * radius * radius
+    return area
+
+print('시작')
+radius = get_radius('넓이를 구하고자 하는 원의 반지름은? ')
+result = get_circle_area(radius)
+print("원의 넓이는:", result)
+print("끝")
