00fe32d70ef330ec8cd31f30646ec3687fec0b7e
samigas <163407588+samigas@users.noreply.github.com>
2024-03-22 16:13:17 +0900

    hw2.py

--- hw2.py
+++ hw2.py
@@ -1,6 +1,6 @@
 def get_integer(prompt):
-    res = int(input(prompt))
-    return res
+        return int(input(prompt))
+    
 
 def exchange(price):
     n500 = price//500
@@ -9,6 +9,5 @@ def exchange(price):
     n10 = (((price%500)%100)%50)//10
     print('500원 짜리는:', n500, '개', '100원 짜리는:', n100, '개', '50원 짜리는:', n50, '개', '10원짜리는:', n10, '개이다')
 
-pri = int(get_integer('동전으로 바꾸고 싶은 금액은?'))
-result= exchange(pri)
-print(result)
+price = get_integer('동전으로 바꾸고 싶은 금액은?')
+exchange(price)
