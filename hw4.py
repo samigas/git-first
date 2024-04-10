84065f067cd571ab5860837b098b70604e85d176
samigas <163407588+samigas@users.noreply.github.com>
2024-04-10 23:59:01 +0900

    hw4.py

--- hw4.py
+++ hw4.py
@@ -4,7 +4,10 @@ def Hello_n(prompt):
     msg2 = 'welcome to seoul'
     N1 = len(msg1)
     N2 = len(msg2)
-    nstr = len(msg1) if (len(msg1) > len(msg2)) else len(msg2)
+    if len(msg1) > len(msg2): 
+        nstr = len(msg1) 
+    else:
+         nstr = len(msg2)
     print('-' * nstr)
     print(msg1)
     print(msg2)
